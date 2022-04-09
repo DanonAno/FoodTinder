9ca6a67e7321e0ef8f6399356982dc9e117b4442
Daniil Luparevich <daniilluparevich@gmail.com>
2022-04-09 12:11:48 +0300

    Initial Commit

---
+++ VezdekodMobileSlider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VezdekodMobileSlider
+//
+//  Created by  Даниил on 09.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
