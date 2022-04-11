6bc73c2877bacf3524008e3680fee9b1f9670eea
Daniil Luparevich <daniilluparevich@gmail.com>
2022-04-11 21:24:23 +0500

    10 balls

--- VezdekodMobileSlider/Models/CardsDataModel.swift
+++ VezdekodMobileSlider/Models/CardsDataModel.swift
@@ -8,14 +8,12 @@
 import UIKit
 struct CardsDataModel {
     
-    var bgColor: UIColor
     var text : String
     var image : UIImage?
     var adress: String
     var time: String
       
     init(bgColor: UIColor, text: String, image: UIImage?, adress: String, time: String) {
-        self.bgColor = bgColor
         self.text = text
         self.image = image
         self.adress = adress
--- VezdekodMobileSlider/Scenes/Main/MainViewController.swift
+++ VezdekodMobileSlider/Scenes/Main/MainViewController.swift
@@ -17,37 +17,40 @@ class MainViewController: UIViewController {
     let cardView: UIView = {
         let view = UIView()
         view.backgroundColor = .white
-        view.layer.cornerRadius = 24
+        view.layer.cornerRadius = 0
+        
         return view
     }()
     let buttonAccept: UIButton = {
-        let button = UIButton()
-        button.setTitle("Приянть", for: .normal)
+        let button = UIButton(type: .system)
+        button.setTitle("Принять", for: .normal)
         button.backgroundColor = .systemGreen
         button.layer.cornerRadius = 5
-       //button.addAction(<#T##action: UIAction##UIAction#>, for: <#T##UIControl.Event#>)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(acceptZakaz), for: .touchUpInside)
         return button
     }()
     let cancelButton: UIButton = {
-        let button = UIButton()
+        let button = UIButton(type: .system)
         button.setTitle("Отклонить", for: .normal)
-        button.backgroundColor = .systemGreen
+        button.backgroundColor = .systemRed
+        button.setTitleColor(.white, for: .normal)
         button.layer.cornerRadius = 5
-        //button.addAction(<#T##action: UIAction##UIAction#>, for: <#T##UIControl.Event#>)
+        button.addTarget(self, action: #selector(setupElements), for: .touchUpInside)
         return button
     }()
     let nameLabel: UILabel = {
        let label = UILabel()
         label.text = "Dodo pizza"
-        label.font = UIFont.systemFont(ofSize: 20)
-        label.textColor = .green
+        label.font = UIFont.italicSystemFont(ofSize: 25)
+        label.textColor = .black
         return label
     }()
     let adressLabel: UILabel = {
         let label = UILabel()
          label.text = "Ломоносова 17"
          label.font = UIFont.systemFont(ofSize: 15)
-         label.textColor = .green
+         label.textColor = .black
          return label
     }()
     let timeLabel: UILabel = {
@@ -56,16 +59,22 @@ class MainViewController: UIViewController {
         label.font = UIFont.systemFont(ofSize: 10)
         return label
     }()
-    let restorauntImage: UIImage = {
-        let image = UIImage(named: "MacDonalds")
-        
-        return image!
+    let restorauntImage: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "DodoPizza")
+//        image.layer.cornerRadius = 24
+        image.layer.masksToBounds = false
+        //image.image.
+        return image
     }()
+    
+    var namesModel = ["Badar", "BrutLe", "DodoPizza","Hinkaly", "MacDonalds", "Ozz"]
     //MARK: - LifeSycle
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red: 185/255, green: 185/255, blue: 185/255, alpha: 1)
         setupUI()
+        makeUILayout()
     }
     //MARK: - SetupLayout
     private func setupUI() {
@@ -73,13 +82,86 @@ class MainViewController: UIViewController {
         view.addSubview(buttonAccept)
         view.addSubview(cancelButton)
         cardView.addSubview(nameLabel)
+        cardView.addSubview(adressLabel)
         cardView.addSubview(timeLabel)
+        cardView.addSubview(restorauntImage)
     }
     
     private func makeUILayout() {
         cardView.snp.makeConstraints { make in
-            make.top.equalTo(<#T##other: ConstraintRelatableTarget##ConstraintRelatableTarget#>)
+            make.center.equalTo(view.snp.center)
+            make.left.right.equalToSuperview().inset(30)
+            make.height.equalTo(400)
         }
+        nameLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(cardView.snp.bottom).inset(50)
+            make.left.equalTo(cardView.snp.left).inset(12)
+        }
+        adressLabel.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(4)
+            make.left.equalTo(cardView.snp.left).inset(12)
+        }
+        timeLabel.snp.makeConstraints { make in
+            make.top.equalTo(adressLabel.snp.bottom).offset(4)
+            make.left.equalTo(cardView.snp.left).inset(12)
+        }
+        restorauntImage.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.left.right.equalToSuperview()
+            make.bottom.equalTo(nameLabel).inset(50)
+        }
+        
+        buttonAccept.snp.makeConstraints { make in
+            make.top.equalTo(cardView.snp.bottom).offset(10)
+            make.left.equalToSuperview().inset(50)
+            make.height.equalTo(50)
+            make.width.equalTo(cancelButton.snp.width)
+        }
+        cancelButton.snp.makeConstraints { make in
+            make.top.equalTo(cardView.snp.bottom).offset(10)
+            make.left.equalTo(buttonAccept.snp.right).offset(50)
+            make.height.equalTo(50)
+        }
+        
+    }
+    
+    @objc private func setupElements() {
+        let randomLogic = namesModel.randomElement()
+        switch randomLogic {
+        case "Badar":
+            nameLabel.text = "Badar"
+            adressLabel.text = "Проспект победы 12"
+            restorauntImage.image = UIImage(named: randomLogic!)
+        case "BrutLe":
+            nameLabel.text = "Брют ли"
+            adressLabel.text = "Университетская набережная 12"
+            restorauntImage.image = UIImage(named: randomLogic!)
+        case "DodoPizza":
+            nameLabel.text = "Dodo Pizza"
+            adressLabel.text = "Ломоносова 17"
+            restorauntImage.image = UIImage(named: randomLogic!)
+        case "Hinkaly":
+            nameLabel.text = "Хинкали и Вино"
+            adressLabel.text = "Где то рядом с Юургу"
+            restorauntImage.image = UIImage(named: randomLogic!)
+        case "MacDonalds":
+            nameLabel.text = "Мак дональдс"
+            adressLabel.text = "Кирова 21"
+            restorauntImage.image = UIImage(named: randomLogic!)
+        case "Ozz":
+            nameLabel.text = "Ozz"
+            adressLabel.text = "Энтузиастов 11"
+            restorauntImage.image = UIImage(named: randomLogic!)
+        default:
+            break
+            
+        }
+        reloadInputViews()
+    }
+    @objc private func acceptZakaz() {
+        var dialogMessage = UIAlertController(title: "Attention", message: "Ваш заказ принят в обработку.", preferredStyle: .alert)
+        self.present(dialogMessage, animated: true, completion: nil)
+        
     }
 }
 
