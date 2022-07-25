42d1d55edf507cbe475acde90e1d7d727b10f3af
Daniil Luparevich <daniilluparevich@gmail.com>
2022-07-25 23:42:19 +0500

    feat: Added Constants

---
+++ VezdekodMobileSlider/Constants/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  VezdekodMobileSlider
+//
+//  Created by  Даниил on 25.07.2022.
+//
+
+import Foundation
+
+struct Constants {
+    let yandexApiKey = "05aaa8d6-b0e4-44c7-88f9-45f27f5e6071"
+}
